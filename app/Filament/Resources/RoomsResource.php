efecd1c32e42743ba42512117da440a812ac7a75
jordan <jordantsap@hotmail.gr>
2024-10-30 10:32:08 +0200

    RoomsResource DeleteAction::make(),

--- app/Filament/Resources/RoomsResource.php
+++ app/Filament/Resources/RoomsResource.php
@@ -60,6 +60,7 @@ class RoomsResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
